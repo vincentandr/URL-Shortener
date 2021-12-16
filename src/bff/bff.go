0db1b2058c168879d3bab707edd3d68fbf7fc42e
Vincent <vincentandrian26@gmail.com>
2021-12-16 22:15:17 +0800

    added redis, prepared project structure

---
+++ src/bff/bff.go
@@ -0,0 +1,36 @@
+package bff
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var port string = ":3000"
+
+type Router struct {
+	*mux.Router
+}
+
+// Router constructor
+func NewRouter() *Router{
+	r := &Router{
+		Router: mux.NewRouter(),
+	}
+	r.routes()
+	return r
+}
+
+func (r *Router) routes() {
+	r.HandleFunc("/", Home).Methods("GET")
+	r.HandleFunc("/getProducts", GetProducts).Methods("GET")
+}
+
+func main() {
+	fmt.Println("Server starting at port " + port)
+
+	r := NewRouter()
+
+	http.ListenAndServe(port, r)
+}
